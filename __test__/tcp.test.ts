05013bb69be5e6b7255dfb8dba46fc0bd6a094d9
Shing <sunquakes@outlook.com>
2024-01-06 22:10:25 +0800

    server add discovery.

--- __test__/tcp.test.ts
+++ __test__/tcp.test.ts
@@ -80,9 +80,11 @@ test('Tcp protocol client call server.', async () => {
 
 test('Tcp consul.', async () => {
   const port = 4002
+  const discovery = new Consul('http://localhost:8500')
   // Start a server.
   const s: net.Server = await new Promise((resolve) => {
-    const server = NewServer('tcp', port)
+    ;(json as jest.Mock).mockResolvedValue('')
+    const server = NewServer('tcp', port, discovery)
     server.register(new Rpc())
     server.start((s: net.Server) => {
       resolve(s)
@@ -90,7 +92,6 @@ test('Tcp consul.', async () => {
   })
 
   // Call server.
-  const discovery = new Consul('http://localhost:8500')
   ;(json as jest.Mock).mockResolvedValue(
     `[{"AggregatedStatus":"passing","Service":{"ID":"java_http-2:${port}","Service":"java_http","Tags":[],"Meta":{},"Port":${port},"Address":"localhost","TaggedAddresses":{"lan_ipv4":{"Address":"localhost","Port":${port}},"wan_ipv4":{"Address":"localhost","Port":${port}}},"Weights":{"Passing":1,"Warning":1},"EnableTagOverride":false,"Datacenter":"dc1"},"Checks":[{"Node":"1ae846e40d15","CheckID":"service:java_http-2:${port}","Name":"Service 'java_http' check","Status":"passing","Notes":"","Output":"HTTP GET http://localhost:${port}: 200 OK Output: ","ServiceID":"java_http-2:${port}","ServiceName":"java_http","ServiceTags":null,"Type":"","ExposedPort":0,"Definition":{"Interval":"0s","Timeout":"0s","DeregisterCriticalServiceAfter":"0s","HTTP":"","Header":null,"Method":"","Body":"","TLSServerName":"","TLSSkipVerify":false,"TCP":"","UDP":"","GRPC":"","GRPCUseTLS":false},"CreateIndex":0,"ModifyIndex":0}]}]`
   )
@@ -110,9 +111,11 @@ test('Tcp consul.', async () => {
 
 test('Tcp nacos.', async () => {
   const port = 4003
+  const discovery = new Nacos('http://localhost:8848')
   // Start a server.
   const s: net.Server = await new Promise((resolve) => {
-    const server = NewServer('tcp', port)
+    ;(json as jest.Mock).mockResolvedValue('ok')
+    const server = NewServer('tcp', port, discovery)
     server.register(new Rpc())
     server.start((s: net.Server) => {
       resolve(s)
@@ -120,7 +123,6 @@ test('Tcp nacos.', async () => {
   })
 
   // Call server.
-  const discovery = new Nacos('http://localhost:8848')
   ;(get as jest.Mock).mockResolvedValue(
     `{"name":"DEFAULT_GROUP@@java_tcp","groupName":"DEFAULT_GROUP","clusters":"","cacheMillis":10000,"hosts":[{"instanceId":"localhost#${port}#DEFAULT#DEFAULT_GROUP@@java_tcp","ip":"localhost","port":${port},"weight":1.0,"healthy":true,"enabled":true,"ephemeral":true,"clusterName":"DEFAULT","serviceName":"DEFAULT_GROUP@@java_tcp","metadata":{},"instanceHeartBeatInterval":5000,"instanceHeartBeatTimeOut":15000,"ipDeleteTimeout":30000,"instanceIdGenerator":"simple"}],"lastRefTime":1673444367069,"checksum":"","allIPs":false,"reachProtectionThreshold":false,"valid":true}`
   )
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import TcpServer from './server/tcp'
 import HttpServer from './server/http'
 import Server from 'server/server'
+import Driver from 'discovery/driver'
 
-export default function NewServer(protocol: string, port: number): Server {
+export default function NewServer(protocol: string, port: number, discovery?: Driver): Server {
   let server
   switch (protocol) {
     case 'tcp':
-      server = new TcpServer(port)
+      server = new TcpServer(port, discovery)
       break
     case 'http':
-      server = new HttpServer(port)
+      server = new HttpServer(port, discovery)
       break
     default:
       throw new Error('Unknown error')
